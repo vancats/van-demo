579032ca4f889df47cf12d00b22abfbec2e438a2
vancats <leiqifan@163.com>
2022-08-21 22:43:36 +0800

    feat: css

--- 5.Vue/index.js
+++ 5.Vue/index.js
@@ -1,33 +1,33 @@
 const compiler = require('vue-template-compiler')
 const v_if = compiler.compile(`
-<div id="test">
+<div>
   <p v-if="false">测试</p>
 </div>
 `)
 console.log('----v-if----')
 console.log(v_if.render)
 // with (this) {
-//   return _c('div', { attrs: { "id": "test" } }, [
+//   return _c('div',[
 //     (false) ? _c('p', [_v("测试")]) : _e()
 //   ])
 // }
 
 const v_show = compiler.compile(`
-<div id="test">
+<div>
   <p v-show="false">测试</p>
 </div>
 `)
 console.log('----v-show----')
 console.log(v_show.render)
 // with (this) {
-//   return _c('div', { attrs: { "id": "test" } }, [
+//   return _c('div',[
 //     _c('p', {
 //       directives: [{ name: "show", rawName: "v-show", value: (false), expression: "false" }]
 //     }, [_v("测试")])])
 // }
 
 const v_for_if = compiler.compile(`
-<div id="test">
+<div>
   <ul>
     <li v-for="ind in 5" v-if="ind === 5">
       {{ ind }}
@@ -38,7 +38,7 @@ const v_for_if = compiler.compile(`
 console.log('----v-for----')
 console.log(v_for_if.render)
 // with (this) {
-//   return _c('div', { attrs: { "id": "test" } }, [
+//   return _c('div',[
 //     _c('ul',
 //       _l((5), function (ind) {
 //         return (ind === 5) ? _c('li', [_v("\n      " + _s(ind) + "\n    ")]) : _e()
@@ -47,14 +47,14 @@ console.log(v_for_if.render)
 // }
 
 const v_html = compiler.compile(`
-<div id="test">
+<div>
   <p v-html="'<div>23231213{{title}}<div>'">123</p>
 </div>
 `)
 console.log('----v-html----')
 console.log(v_html.render)
 // with (this) {
-//   return _c('div', { attrs: { "id": "test" } }, [
+//   return _c('div',[
 //     _c('p', {
 //       domProps: { "innerHTML": _s('<div>23231213{{title}}<div>') }
 //     }, [_v("123")])
@@ -63,14 +63,14 @@ console.log(v_html.render)
 
 
 const v_text = compiler.compile(`
-<div id="test">
+<div>
   <p v-text="'<div>23231213{{title}}<div>'">123</p>
 </div>
 `)
 console.log('----v-text----')
 console.log(v_text.render)
 // with (this) {
-//   return _c('div', { attrs: { "id": "test" } }, [
+//   return _c('div',[
 //     _c('p', {
 //       domProps: { "textContent": _s('<div>23231213{{title}}<div>') }
 //     }, [_v("123")])
@@ -78,14 +78,14 @@ console.log(v_text.render)
 // }
 
 const v_model = compiler.compile(`
-<div id="test">
+<div>
   <input type="checkbox" v-model="msg" />
 </div>
 `)
 console.log('----v-model----')
 console.log(v_model.render)
 // with (this) {
-//   return _c('div', { attrs: { "id": "test" } }, [
+//   return _c('div',[
 //     _c('input', {
 //       directives: [{ name: "model", rawName: "v-model", value: (msg), expression: "msg" }],
 //       domProps: { "value": (msg) },
@@ -99,8 +99,23 @@ console.log(v_model.render)
 //   ])
 // }
 
+const v_model_el = compiler.compile(`
+  <el-checkbox v-model="check"></el-checkbox>
+`)
+console.log('----v-model-el----')
+console.log(v_model_el.render)
+// with (this) {
+//   return _c('el-checkbox',
+//     {
+//       model: {
+//         value: (check),
+//         callback: function ($$v) { check = $$v }, expression: "check"
+//       }
+//     })
+// }
+
 const v_on = compiler.compile(`
-<div>
+<div></div>
   <div @click="add"></div>
   <comp @click="add" @click.native="remove"></comp>
 </div>
